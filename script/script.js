eadadb469b91c85ea4ee0a7e1cb2f6c969968c58
Dudarev <dima_dudariev@mail.ru>
2020-02-25 17:20:23 +0300

    Добавил калькулятор и анимацию  счета

--- script/script.js
+++ script/script.js
@@ -301,28 +301,76 @@ window.addEventListener('DOMContentLoaded', () => {
     slider();
 
     //Расчитать стоимость
+    const calc = (price = 100) => {
 
-    const totalArea = document.querySelector('.calc-square');
-    const numberPremises = document.querySelector('.calc-count');
-    const term = document.querySelector('.calc-day');
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcCount = document.querySelector('.calc-count'),
+            calcDay = document.querySelector('.calc-day'),
+            totalValue = document.getElementById('total');
 
-    const checkNumb = (value) => {
-        value.value = value.value.replace(/[^\d]/g, '');
-    };
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value;
+            const squareValue = +calcSquare.value;
 
-    totalArea.addEventListener('input', (event) => {
-        checkNumb(event.target);
-    });
+            if(calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
 
-    numberPremises.addEventListener('input', (event) => {
-        checkNumb(event.target);
-    });
+            if(calcDay.value < 5 && calcDay.value) {
+                dayValue *= 2;
+            }else if(calcDay.value < 10 && calcDay.value) {
+                dayValue *= 1.5;
+            }
 
-    term.addEventListener('input', () => {
-        checkNumb(event.target);
-    });
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+                let temp = 0;
+                let id = setInterval(() => {
+                    temp += 10;
+                    if(temp <= total){
+                    totalValue.textContent = temp;
+                    }else{
+                        clearInterval(id);
+                    }
+                }, 1);
+            }else{
+                total = 0;
+            }
+
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+            let target = event.target;
+
+            if(target.matches('.calc-type') || target.matches('.calc-square') || 
+            target.matches('.calc-count') || target.matches('.calc-day')) {
+                countSum();
+            }
+        })
     
-
+        const checkNumb = (value) => {
+            value.value = value.value.replace(/[^\d]/g, '');
+        };
+    
+        calcSquare.addEventListener('input', (event) => {
+            checkNumb(event.target);
+        });
+    
+        calcCount.addEventListener('input', (event) => {
+            checkNumb(event.target);
+        });
+    
+        calcDay.addEventListener('input', () => {
+            checkNumb(event.target);
+        });
+        
+    };
+    calc(100);
     //Наша команда
 
     let firstImg;
